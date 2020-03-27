0e2c67cc394a16583a7ceeaf5046b462635eca91
Seth Greenbaum <sethmgreenbaum@gmail.com>
2020-03-26 20:02:19 -0600

    added sequelize and api routes

---
+++ models/post.js
@@ -0,0 +1,29 @@
+module.exports = function(sequelize, DataTypes) {
+  var Post = sequelize.define("Post", {
+    header: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    link: {
+      type: DataTypes.STRING
+    },
+    category: {
+      type: DataTypes.STRING
+    },
+    userid: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  });
+  return Post;
+};
