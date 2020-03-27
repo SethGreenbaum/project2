8d3765a1bc31bca9eb38366c50e4f852ebc1c300
Seth Greenbaum <sethmgreenbaum@gmail.com>
2020-03-26 18:25:09 -0600

    added Oauth for twitch

--- public/js/members.js
+++ public/js/members.js
@@ -4,4 +4,21 @@ $(document).ready(function() {
   $.get("/api/user_data").then(function(data) {
     $(".member-name").text(data.email);
   });
+  var twitch = $(".twitch");
+  var clientId = "udzdn6yna0fjfyrq4luu79nmxr0o3n";
+  var redirectUri = window.location.href;
+  var returnedAuthorizationToken;
+  var authorizationToken;
+  twitch.on("click", function(event){
+    event.preventDefault();
+    var authUrl = `https://id.twitch.tv/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&scope=viewing_activity_read`;
+    window.location.href = authUrl;
+  });
+  function getAuthorizationToken(){
+    returnedAuthorizationToken = location.hash.substr(1);
+    authorizationToken = "Bearer "+returnedAuthorizationToken.substring(returnedAuthorizationToken.indexOf("=")+1,returnedAuthorizationToken.indexOf("&"));
+    console.log(authorizationToken);
+  };
+  getAuthorizationToken();
+
 });
