c1f1e78a5ee023dfa1070b87f1dc55354b226cb3
Kauri Nanda <bknanda6@gmail.com>
2020-03-28 13:49:48 -0600

    made signup route, moved jquery cdn, currently trying to fix css pathing

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -32,7 +32,6 @@ module.exports = function(app) {
     res.render("signup", {});
   });
 
-
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, function(req, res) {
